cbda4ddc8b9a06f05d5146b849b3338c00cae2bb
tanushreegomathi-cloud <tanushreegomathi@gmail.com>
2026-02-08 01:38:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 const noBtn = document.getElementById("noBtn");
 const heartsContainer = document.getElementById("hearts");
 
-function openValentine() {
-  document.getElementById("start").style.display = "none";
-  document.getElementById("valentine").classList.remove("hidden");
-}
-
 noBtn.addEventListener("mouseover", moveNo);
 noBtn.addEventListener("click", moveNo);
 
 function moveNo() {
   noBtn.style.position = "absolute";
-  noBtn.style.left = Math.random() * 80 + "vw";
-  noBtn.style.top = Math.random() * 80 + "vh";
+  noBtn.style.left = Math.random() * 70 + "vw";
+  noBtn.style.top = Math.random() * 70 + "vh";
 }
 
 function yesClicked() {
-  document.getElementById("valentine").innerHTML =
-    "<h1>YAY 💖 You’re my Valentine 🥰</h1>";
+  document.getElementById("valentine").innerHTML = `
+    <h1>YAYYY ❤️ you're my galentine 💃</h1>
+    <h2>love you achu kuttyy 🥺💞</h2>
+  `;
 
-  setInterval(createHeart, 300);
+  setInterval(createHeart, 200);
 }
 
 function createHeart() {
   const heart = document.createElement("div");
   heart.classList.add("heart");
-  heart.innerText = "💖";
+  heart.innerText = "❤️";
   heart.style.left = Math.random() * 100 + "vw";
   heartsContainer.appendChild(heart);
 
